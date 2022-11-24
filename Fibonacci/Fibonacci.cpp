a40c6eecfe0f94d4c7665521eebc18fa501a057a
DESKTOP-B9S7PAR\Angel <angelgmorenor@gmail.com>
2022-11-24 12:18:44 -0400

    fibonacci

--- Fibonacci/Fibonacci.cpp
+++ Fibonacci/Fibonacci.cpp
@@ -3,22 +3,48 @@
 
 using namespace std;
 
-int fibonacci(int serieAnterior, int serie, int n) {
+int Fibonacci(int serieAnterior, int serie, int n) {
 	int aux = 0;
-	if (n == 0)
+		if (n == 0)
+		{
+			return serieAnterior;
+		}
+		else {
+			aux = serie;
+			serie = serieAnterior + serie;
+			Fibonacci(aux, serie, n - 1);
+		}
+}
+int EsFibonacci(int serieAnt, int serie, int n) {
+	int aux = 0;
+	if (n >= serieAnt)
 	{
-		return serieAnterior;
+		if (serie == n || serieAnt==n) {
+			cout << "Exito " << n << " forma parte de la serie de fibonacci\n";
+			return serie;
+		}
+		else
+		{
+			aux = serie;
+			serie = serieAnt + serie;
+			EsFibonacci(aux, serie, n);
+		}
 	}
 	else {
-		aux = serie;
-		serie = serieAnterior + serie;
-		fibonacci(aux, serie, n - 1);
+		cout << "No aparecio broki\n";
+		return serie;
 	}
 }
-
 int main() {
 
-	cout << fibonacci(0, 1, 5);
+	cout << "\n\n *--* Funcion que dice si un numero pertenece a la serie y el ultimo elemento de la serie que calculó *--* \n\n";
+	cout << "El ultimo numero de la serie calculado es: " << EsFibonacci(0, 1, 0) << endl;
+	cout << "El ultimo numero de la serie calculado es: " << EsFibonacci(0, 1, 1597) << endl;
+
+	cout << "\n\n *--* Funcion que devuelve el numero de la serie de fibonacci en la posicion solicitada *--* \n\n";
+	int posicion = 0;
+	cout << "El numero en la posicion " << posicion << " es: " << Fibonacci(0, 1, posicion);
+	cout << "El numero en la posicion " << posicion + 8 << " es: " << Fibonacci(0, 1, posicion + 8);
 	return 0;
 }
 
