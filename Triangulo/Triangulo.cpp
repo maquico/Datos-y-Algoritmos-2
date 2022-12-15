82613c08326a67db741a2e7f6b3e7f42b6ceb61a
DESKTOP-B9S7PAR\Angel <angelgmorenor@gmail.com>
2022-12-15 16:32:15 -0400

    Creacion triangulo, comentario circulo

---
+++ Triangulo/Triangulo.cpp
@@ -0,0 +1,24 @@
+/* LABORATORIO ESTRUCTURAS DE DATOS Y ALGORITMOS II
+* ENUNCIADO: TAREA TRIANGULO
+* ASUNTO: ESTE PROGRAMA GENERA UN FRACTAL
+AUTORES: ANGEL MORENO ID:1104666
+*        GLEIDY ESPINAL ID:1104225
+*        ALLEN SILVERIO ID:1104220
+*        CRISTIAN JIMÉNEZ ID:1106166
+*        WILLIAM FERREIRA ID:1104228
+* FECHA: 15/12/2022
+*/
+#include <iostream> 
+#include <string>
+#include <conio.h>
+#include <math.h>
+#include <windows.h>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+
+	return 0;
+}
