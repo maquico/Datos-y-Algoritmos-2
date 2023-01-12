d6d2af5b70cfca15dedd1ece8ad5f9369bd56342
DESKTOP-B9S7PAR\Angel <angelgmorenor@gmail.com>
2023-01-12 01:49:41 -0400

    D U R O

--- TicTacToeConAlgoritmoEnConsola/TicTacToeConAlgoritmoEnConsola.cpp
+++ TicTacToeConAlgoritmoEnConsola/TicTacToeConAlgoritmoEnConsola.cpp
@@ -9,12 +9,15 @@
 using namespace std;
 COORD posAmenazada;
 int dimension = 3;
-int tablero[3][3] = { {0,0,0}, {0,0,0}, {0,0,0} };
+int bot = 1;
+int oponente = 0;
+char tablero[3][3] = { {' ',' ',' '}, {' ',' ',' '}, {' ',' ',' '} };
 bool partidaTerminada = false, botX=false;
 int jugadas = 0;
 char simboloOponente, simboloBot;
+int posXbot, posYbot;
 
-#define DIMENSION 3;
+//#define DIMENSION 3;
 
 void GoToXY(int x, int y)
 {//La función coloca el cursor para imprimir en el lugar que queramos.
@@ -26,20 +29,20 @@ void GoToXY(int x, int y)
 	dwPos.Y = y;
 	SetConsoleCursorPosition(hCon, dwPos);
 }
-
-char ConvertirNumSimbolo(int num) {
-	switch (num) {
-	case 0:
-		return ' ';
-		break;
-	case 1:
-		return simboloOponente;
-		break;
-	case -1:
-		return simboloBot;
-		break;
-	}
-}
+//
+//char ConvertirNumSimbolo(int num) {
+//	switch (num) {
+//	case 0:
+//		return ' ';
+//		break;
+//	case 1:
+//		return simboloOponente;
+//		break;
+//	case -1:
+//		return simboloBot;
+//		break;
+//	}
+//}
 
 void TableroVisual() 
 {
@@ -59,19 +62,28 @@ void TableroVisual()
 				
 				GoToXY(posX, posY);
 				posX += 4;
-				cout << ConvertirNumSimbolo(tablero[i][j]);
+				cout << (tablero[i][j]);
 			}
 			posX = 3;
 			posY = 2+((i + 1) * 2);
 		}
 }
 
-void ColocarJugada (int posX, int posY, bool jugadaUsuario){
-	(jugadaUsuario) ? tablero[posX][posY] = 1 : tablero[posX][posY] = -1;
-	TableroVisual();
-	jugadas++;
-}
+//void ColocarJugada (int posX, int posY, bool jugadaUsuario){
+//	(jugadaUsuario) ? tablero[posX][posY] = simboloOponente : tablero[posX][posY] = -1;
+//	TableroVisual();
+//	jugadas++;
+//}
 
+void Borrar(char tablero[3][3])
+{
+	// Initially the board is empty 
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < SIDE; j++)
+			tablero[i][j] = ' ';
+	}
+}
 
 bool RevisarFilas(char tablero[][3])
 {
@@ -107,7 +119,7 @@ bool HayVictoria(char tablero[3][3]) {
 
 void Ganador(int Turno)
 {
-	if (Turno == Computadora)
+	if (Turno == bot)
 		printf("La COMPUTADORA ha ganado\n");
 	else
 		printf("El HUMANO ha ganado\n");
@@ -179,43 +191,142 @@ int minimax(char tablero[3][3], int profundidad, bool esBot)
 	}
 }
 
-int main()
+// Function to calculate best move
+void MejorMovimiento(char tablero[3][3], int jugada)
 {
-	int opcionSimbolo;
-	cout << "Que simbolo quiere ser?:\n\n";
-	cout << "1) X (Empieza)\n";
-	cout << "2) O (Es Segundo)\n\n";
-	cout << "Opcion Elegida: ";
-	cin >> opcionSimbolo;
-
-	if(opcionSimbolo == 1) {
-		simboloBot = 'O';
-		simboloOponente = 'X';
-	}
-	else 
+	int x = -1, y = -1;
+	int puntuacion = 0, mejorPuntuacion = -999;
+	for (int i = 0; i < 3; i++)
 	{
-		botX = true;
-		simboloBot = 'X';
-		simboloOponente = 'O';
-	}
-	int posX, posY;
-	
-	_getch();
-	do {
-
-		TableroVisual();
-		if (botX) {
-			
-			botX = false;
+		for (int j = 0; j < 3; j++)
+		{
+			if (tablero[i][j] == ' ')
+			{
+				tablero[i][j] = simboloBot;
+				puntuacion = minimax(tablero, jugada+1, false);
+				tablero[i][j] = ' ';
+				if(puntuacion > mejorPuntuacion)
+				{
+					mejorPuntuacion = puntuacion;
+					x = i;
+					y = j;
+				}
+			}
 		}
-		else {
+	}
+	posXbot = x;
+	posYbot = y;
+}
+
+void jugar(int turno)
+{
+	int posX, posY;
+	/*char tablero[3][3];*/
+	int jugadas = 0; /*x = 0, y = 0;*/
+
+	TableroVisual();
+
+	while (HayVictoria(tablero) == false && jugadas != 9)
+	{
+		//int n;
+		if (turno == bot)
+		{
+			//n = MejorMovimiento(tablero, jugadas);
+			//x = n / 3;
+			//y = n % 3;
+			MejorMovimiento(tablero, jugadas);
+			tablero[posXbot][posYbot] = simboloBot;
+			GoToXY(20,20);
+			cout << "COMPUTER has put a " << simboloBot << "in cell [" << posXbot << "," << posYbot << "]";
+			TableroVisual();
+			jugadas++;
+			turno = oponente;
+		}
+
+		else if (turno == oponente)
+		{
 			cout << "\n\nIngrese la Posicion en X: ";
 			cin >> posX;
 			cout << "\nIngrese la Posicion en Y: ";
 			cin >> posY;
-			ColocarJugada(posX, posY, true);
 			
+			/*n--;
+			x = n / 3;
+			y = n % 3;*/
+			if (tablero[posX][posY] == ' ' && (posX < 3 && posX >= 0) && (posY < 3 && posY >= 0))
+			{
+				tablero[posX][posY] = simboloOponente;
+				GoToXY(50,50);
+				cout << "HUMAN has put a " << simboloOponente << "in cell [" << posX << "," << posY << "]";
+				TableroVisual();
+				jugadas++;
+				turno = bot;
+			}
+			else if (tablero[posX][posY] != ' ')
+			{
+				GoToXY(20,20);
+				cout << "Posicion Ocupada, seleccione otra en un lugar disponible";
+			}
+			else
+			{
+				cout << "\n Posicion Invalida, fuera de rango \n";
+			}
 		}
-	} while (jugadas != 9);
+	}
+
+	// Empate
+	if (HayVictoria(tablero) == false && jugadas == 9)
+		cout << "DUROSSSSS EMPATE";
+	else
+	{
+		// Declarar el ganador actual
+		if (turno == bot)
+			turno = oponente;
+		else if (turno == oponente)
+			turno = bot;
+
+		Ganador(turno);
+	}
 }
 
+int main()
+{
+	int opcionSimbolo;
+	bool cerrar = false;
+	do{
+		system("cls");
+	cout << "Que simbolo quiere ser?:\n\n";
+	cout << "1) X (Empieza)\n";
+	cout << "2) O (Es Segundo)\n";
+	cout << "3) Salir\n\n";
+	cout << "Opcion Elegida: ";
+	cin >> opcionSimbolo;
+
+	
+
+	switch (opcionSimbolo) {
+	case 1: 
+		simboloBot = 'O';
+		simboloOponente = 'X';
+		jugar(oponente);
+		_getch();
+		break;
+	case 2:
+		botX = true;
+		simboloBot = 'X';
+		simboloOponente = 'O';
+		jugar(bot);
+		break;
+	case 3:
+		cerrar = true;
+		GoToXY(20, 20);
+		cout << "Cerrando......";
+		break;
+	default:
+		cout << "Esa opcion no esta en el menu ";
+		break;
+	}
+	} while (!cerrar);
+		_getch();
+
+}
